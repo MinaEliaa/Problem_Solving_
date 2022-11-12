b3000e051c1464f7477421a5d962b17894f4d211
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2022-11-12 21:55:41 +0200

    add files

--- Level 1/Codefores/Bachgold Problem-Round 388 A/main.cpp
+++ Level 1/Codefores/Bachgold Problem-Round 388 A/main.cpp
@@ -2,27 +2,19 @@
 
 using namespace std;
 
-int main()
-{
-    int n ;
-    cin>>n;
-    int count =n/2;
+int main() {
+    int n;
+    cin >> n;
 
-    if (n%2==0){
-
-        for (int i =0 ;i<count;i++){
-            cout<<2<<" ";
-        }
-
-        else{
-
-            for (int i=0;i<count-1 ;i++){
-
-                count<<2<<" ";
-            }
-            cout<<3<<" ";
-        }
+    if(n % 2 == 1) {
+        n /= 2;
+        n--;
+        cout << n + 1 << endl << 3 << ' ';
+    } else {
+        n /= 2;
+        cout << n << endl;
     }
 
-    return 0;
+    for(int i = 0; i < n; i++) cout << 2 << ' ';
+    cout << endl;
 }
