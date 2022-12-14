4dae8ea798bc7a226f8b1b5d0e98adbe79bdf81b
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2022-12-14 13:33:57 +0200

    add

--- Level 1/Codefores/Level 1/Watermelon/main.cpp
+++ Level 1/Codefores/Level 1/Watermelon/main.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int w;
+
+    cin>>w;
+
+    if (w%2==0 && w!=2){
+
+        cout<<"YES"<<endl;
+    }
+
+
+    else cout<<"NO"<<endl;
+
     return 0;
+
 }
