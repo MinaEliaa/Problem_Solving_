ea0ec22d3f6ba1059b6cab591694f73aaa8f1e52
Mina Elia Louize <107637818+MinaEliaa@users.noreply.github.com>
2022-12-14 14:28:55 +0200

    add files

---
+++ Level 1/Codefores/Level 1/Blackjack/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+
+        if (n<=10){
+        cout<<0<<endl;
+        }
+        else if(n<20){
+            cout<<4<<endl;
+        }
+        else if(n==20){
+            cout<<15<<endl;
+        }
+      //else if (n>20 && n!=25){
+         // cout<<19<<endl;
+      //}
+        else {
+                cout<<0<<endl;
+
+}
+    return 0;
+}
